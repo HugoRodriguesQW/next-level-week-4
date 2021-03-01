4f5bf1e2421e7479102142ed509a771a8e6e1ea5
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-03-01 22:59:54 +0000

    Preparo

--- moveit-whats/src/pages/_app.tsx
+++ moveit-whats/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/global.css';
+import { PageMenuContextProvider } from '../contexts/PageMenuContext';
 
 
 function MyApp({ Component, pageProps}) {
 
   return (
+    <PageMenuContextProvider>
       <Component {...pageProps} />
+    </PageMenuContextProvider>
   )
 }
 
