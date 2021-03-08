afdae515fa951bed8a112aee3dedfdeac6df91a6
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-03-08 17:38:31 +0000

    Fixar barra de progresso

--- moveit-whats/src/components/ExperienceBar.tsx
+++ moveit-whats/src/components/ExperienceBar.tsx
@@ -1,16 +1,22 @@
 import styles from '../styles/components/ExperienceBar.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { challengesContext } from '../contexts/ChallengesContext';
 
 export function ExperienceBar() {
   const {currentExperience, experienceToNextLevel} = useContext(challengesContext)
   const percentToNextLevel = Math.round( (currentExperience / experienceToNextLevel) * 100 )
 
+  useEffect(()=> {
+  if( document ) {
+  document.documentElement.style.setProperty('--size-bar', `${percentToNextLevel}%`);
+  }
+  }, [percentToNextLevel])
+
   return (
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{width : `${percentToNextLevel}%`}} />
+        <div />
         <span className={styles.currentExperience} style={{left : `${percentToNextLevel}%`}}>{currentExperience} xp</span>
       </div>
       <span>{experienceToNextLevel} xp</span>
