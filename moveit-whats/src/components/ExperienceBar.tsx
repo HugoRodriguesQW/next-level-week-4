8b2703bcb8ea552eea834ba3027541621d1dcff9
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-03-08 18:46:47 +0000

    Adiciona animação na barra de exp

--- moveit-whats/src/components/ExperienceBar.tsx
+++ moveit-whats/src/components/ExperienceBar.tsx
@@ -1,16 +1,20 @@
 import styles from '../styles/components/ExperienceBar.module.css';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { challengesContext } from '../contexts/ChallengesContext';
 
 export function ExperienceBar() {
   const {currentExperience, experienceToNextLevel} = useContext(challengesContext)
   const percentToNextLevel = Math.round( (currentExperience / experienceToNextLevel) * 100 )
+  const [experienceBarSize, setExpSize] = useState(0)
 
   useEffect(()=> {
-  if( document ) {
-  document.documentElement.style.setProperty('--size-bar', `${percentToNextLevel}%`);
+  if(document && experienceBarSize <= percentToNextLevel) {
+  setTimeout( () => {
+    setExpSize(experienceBarSize + 1)
+    document.documentElement.style.setProperty('--size-bar', `${experienceBarSize}%`);
+  }, 10 )
   }
-  }, [percentToNextLevel])
+  }, [experienceBarSize, percentToNextLevel])
 
   return (
     <header className={styles.experienceBar}>
