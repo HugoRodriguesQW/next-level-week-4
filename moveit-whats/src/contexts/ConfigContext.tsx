f861f2d4afcea3808b2f0613f9bc85894a8a88e1
HugoRodriguesQW <mailvitorhugosr@gmail.com>
2021-03-09 16:15:48 +0000

    Adiciona alterar username

--- moveit-whats/src/contexts/ConfigContext.tsx
+++ moveit-whats/src/contexts/ConfigContext.tsx
@@ -1,11 +1,15 @@
-import {createContext, ReactNode, useState, useEffect} from 'react'
+import {createContext, ReactNode, useState, useEffect, useContext} from 'react'
 import cookies from 'js-cookie'
+import { userContext } from './UserContext';
+
 
 interface configData {
   sounds: boolean;
   notifications: boolean;
   hideProfileImage: boolean;
   isCurrentSaved: boolean;
+  hasBlockedWords: boolean;
+  checkAndSetNewName: (name: string) => void;
   enableDisableSounds: () => void;
   enableDisableNotifications: () => void;
   showHideProfileImage: () => void;
@@ -23,11 +27,27 @@ export const ConfigContext = createContext({} as configData);
 
 export function ConfigProvider({children, ...rest}: configProviderProps) {
 
+  const {username, changeAndSaveUserName} = useContext(userContext)
+  const [localname, setLocalname] = useState(username)
+
   const [sounds, setSoundsState] = useState(rest.sounds ?? true)
   const [notifications, setNotificationsState] = useState(rest.notifications ?? true)
   const [hideProfileImage, setProfileImageState] = useState(rest.hideProfileImage ?? false)
 
   const [isCurrentSaved, setSavedStatus] = useState(true)
+
+  const [hasBlockedWords, setHasBlockedWords] = useState(false)
+
+  function checkAndSetNewName(name: string) {
+    if( name.toLowerCase() != username.toLowerCase() ) { 
+      setHasBlockedWords(true)
+      return 
+    }
+    setHasBlockedWords(false)
+
+    setLocalname(name)
+    console.info('setado', 'name', name, 'user', username, 'local', localname)
+    }
 
   function enableDisableSounds(){
     setSoundsState(sounds ? false : true)
@@ -35,6 +55,7 @@ export function ConfigProvider({children, ...rest}: configProviderProps) {
   function enableDisableNotifications(){
     setNotificationsState(notifications ? false : true)
   }
+
   function showHideProfileImage(){
     setProfileImageState(hideProfileImage ? false : true)
   }
@@ -43,28 +64,37 @@ export function ConfigProvider({children, ...rest}: configProviderProps) {
     cookies.set('enableSounds', String(sounds))
     cookies.set('enableNotification', String(notifications))
     cookies.set('hideProfileImageStatus', String(hideProfileImage))
+    changeAndSaveUserName(localname)
     setSavedStatus(true)
   }
 
   useEffect(()=> {
+    const userName = cookies.get('username')
     const enableSounds = cookies.get('enableSounds')
     const enableNotification = cookies.get('enableNotification')
     const hideProfileImageStatus = cookies.get('hideProfileImageStatus')
 
-    setSavedStatus( 
+    setSavedStatus(
+      userName == localname &&
       enableSounds == String(sounds) &&
       enableNotification == String(notifications) &&
       hideProfileImageStatus == String(hideProfileImage)
     )
     
-  },[sounds, notifications, hideProfileImage])
+  },[sounds, notifications, hideProfileImage, localname])
+
+  useEffect(()=> {
+    setLocalname(username)
+  }, [username])
 
   return (
     <ConfigContext.Provider value={
       {
       sounds,
       notifications,
+      checkAndSetNewName,
       hideProfileImage,
+      hasBlockedWords,
       enableDisableSounds,
       enableDisableNotifications,
       showHideProfileImage,
