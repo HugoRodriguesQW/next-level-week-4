b4aff041493ad6df666a6564b570a6508713f13a
hugorodriguesqw <mailvitorhugosr@gmail.com>
2021-03-12 13:14:12 +0000

    Apply hideProfileImage

--- moveit-whats/src/components/Profile.tsx
+++ moveit-whats/src/components/Profile.tsx
@@ -2,15 +2,19 @@ import styles from '../styles/components/Profile.module.css';
 import { useContext } from 'react';
 import { challengesContext } from '../contexts/ChallengesContext';
 import { userContext } from '../contexts/UserContext';
+import { ConfigContext } from '../contexts/ConfigContext';
 
 
 export function Profile() {
   const {level} = useContext(challengesContext)
   const {userImage, username} = useContext(userContext)
+  const {hideProfileImage} = useContext(ConfigContext)
+  
   
   return (
     <div className={styles.profileContainer}>
-      <img alt={`${username} Profile`} src={userImage} />
+      <img alt={`${username} Profile`} 
+      src={hideProfileImage? '/favicon.png' : userImage} />
       <div>
         <strong>{username}</strong>
         <p>
