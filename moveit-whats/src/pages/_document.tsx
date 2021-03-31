8210a0cd4365e0aef9e638ea1fc4ed4912e0099d
Hugo Rodrigues <71078903+HugoRodriguesQW@users.noreply.github.com>
2021-03-31 17:49:15 +0000

    SEO

--- moveit-whats/src/pages/_document.tsx
+++ moveit-whats/src/pages/_document.tsx
@@ -10,6 +10,34 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
 
           <meta httpEquiv="origin-trial" content="AqyKmy3iO4yBJwtEuP6OuxmtkakkfThRMrr2c0UZ1Gc+VzhEgVUVxLJfuHW3/yyyR59NnUNv5tC4iaLSc2eFMg8AAAB2eyJvcmlnaW4iOiJodHRwczovL25sdzQtaHVnb3JvZHJpZ3Vlc3F3LnZlcmNlbC5hcHA6NDQzIiwiZmVhdHVyZSI6IlVucmVzdHJpY3RlZFNoYXJlZEFycmF5QnVmZmVyIiwiZXhwaXJ5IjoxNjMzNDc4Mzk5fQ==" />
+        
+          <!-- SEO -->
+          <link rel="canonical" href="https://nlw4-hugorodriguesqw.vercel.app/">
+          <meta name="description" content="Inspirado na Técnica Pomodoro, ajuda você a se concentrar nas tarefas e gerenciar seu tempo.">
+          <meta name="robots" content="index, nofollow, noimageindex">
+          <meta httpEquiv="content-type"  content="text/html;charset=utf-8">
+          <!-- SEO - Google+ -->
+          <meta itemprop="name" content="Move.it">
+          <meta itemprop="description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo.">
+          <meta itemprop="image" content="https://nlw4-hugorodriguesqw.vercel.app/banner.svg">
+          <!-- SEO - Facebook -->
+          <meta property="og:url" content="https://nlw4-hugorodriguesqw.vercel.app/">
+          <meta property="og:site_name" content="Move.it">
+          <meta property="og:title" content="Move.it">
+          <meta property="og:description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo.">
+          <meta property="og:type" content="website">
+          <meta property="og:image" content="https://nlw4-hugorodriguesqw.vercel.app/banner.svg">
+          <meta property="og:image:type" content="image/svg+xml">
+          <meta property="og:image:width" content="1200">
+          <meta property="og:image:height" content="630">
+
+           <!-- SEO - Twitter -->
+          <meta name="twitter:url" content="https://nlw4-hugorodriguesqw.vercel.app/">
+          <meta name="twitter:card" content="summary_large_image">
+          <meta name="twitter:title" content="Move.it">
+          <meta name="twitter:description" content="Inspirado na Técnica Pomodoro, ajuda você a se concentrar nas tarefas e gerenciar seu tempo.">
+          <meta name="twitter:image" content="https://nlw4-hugorodriguesqw.vercel.app/banner.svg">
+          <!-- SEO - END -->    
         </Head>
         <body>
           <Main />
