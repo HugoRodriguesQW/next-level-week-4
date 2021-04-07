fadd9bfa1f8d4bf7d9aaa1c447b3490c3f212481
Hugo Rodrigues <71078903+HugoRodriguesQW@users.noreply.github.com>
2021-04-07 21:49:12 +0000

    Update _document.tsx

--- moveit-whats/src/pages/_document.tsx
+++ moveit-whats/src/pages/_document.tsx
@@ -24,24 +24,24 @@ export default class MyDocument extends Document {
           {/* SEO - Google+ */}
           <meta itemProp="name" content="Move.it"/>
           <meta itemProp="description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-          <meta itemProp="image" content="https://nlw4-hugorodriguesqw.vercel.app/banner.svg"/>
+          <meta itemProp="image" content="https://moveit.hugorodrigues.ml/banner.svg"/>
           {/* SEO - Facebook */}
-          <meta property="og:url" content="https://nlw4-hugorodriguesqw.vercel.app/"/>
+          <meta property="og:url" content="https://moveit.hugorodrigues.ml/"/>
           <meta property="og:site_name" content="Move.it"/>
           <meta property="og:title" content="Move.it"/>
           <meta property="og:description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
           <meta property="og:type" content="website"/>
-          <meta property="og:image" content="https://nlw4-hugorodriguesqw.vercel.app/banner.svg"/>
+          <meta property="og:image" content="https://moveit.hugorodrigues.ml/"/>
           <meta property="og:image:type" content="image/svg+xml"/>
           <meta property="og:image:width" content="1200"/>
           <meta property="og:image:height" content="630"/>
 
            {/* SEO - Twitter */}
-          <meta name="twitter:url" content="https://nlw4-hugorodriguesqw.vercel.app/"/>
+          <meta name="twitter:url" content="https://moveit.hugorodrigues.ml/"/>
           <meta name="twitter:card" content="summary_large_image"/>
           <meta name="twitter:title" content="Move.it"/>
           <meta name="twitter:description" content="Inspirado na Técnica Pomodoro, ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-          <meta name="twitter:image" content="https://nlw4-hugorodriguesqw.vercel.app/banner.svg"/>
+          <meta name="twitter:image" content="https://moveit.hugorodrigues.ml/"/>
           {/* SEO - END */}    
         </Head>
         <body>
