361c91515d3d947b7270559eb82f04a90dd68036
Hugo <mailvitorhugosr@gmail.com>
2021-04-12 16:24:34 +0000

    Ops, fix build error

--- moveit-whats/src/sections/home.tsx
+++ moveit-whats/src/sections/home.tsx
@@ -11,8 +11,6 @@ import styles from '../styles/pages/Home.module.css';
 import { useContext } from "react";
 import { userContext } from "../contexts/UserContext";
 
-import {Helmet} from 'react-helmet'
-
 export function HomeApp () {
   const {currentPage} = useContext(userContext)
   return (
