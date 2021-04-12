9f9f4eb3ccc45d2d37d0232de5c90b818b305f91
Hugo <mailvitorhugosr@gmail.com>
2021-04-12 16:13:31 +0000

    Improve SEO 2

--- moveit-whats/src/pages/_app.tsx
+++ moveit-whats/src/pages/_app.tsx
@@ -1,42 +1,13 @@
 import '../styles/global.css';
 import {UserContextProvider} from '../contexts/UserContext';
-import {Helmet} from 'react-helmet'
 function MyApp({ Component, pageProps}) {
 
   return (
-    <>
+    <div className="application">
     <UserContextProvider>
     <Component {...pageProps} />
     </UserContextProvider>
-
-    <Helmet>
-      <title>Move.it - Pomodoro Timer</title>
-      <meta charSet="UTF-8"/>
-      <meta name="title" content="Moveit"/>
-      <meta name="description" content="Inspirado na Técnica Pomodoro, ajuda você a se concentrar nas tarefas e gerenciar seu tempo." />
-      <meta name="robots" content="index, nofollow"/>
-
-      <meta itemProp="name" content="Move.it"/>
-      <meta itemProp="description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-      <meta itemProp="image" content="https://moveit.hugorodrigues.ml/banner.svg"/>
-
-      <meta property="og:url" content="https://moveit.hugorodrigues.ml/"/>
-      <meta property="og:site_name" content="Moveit"/>
-      <meta property="og:title" content="Moveit"/>
-      <meta property="og:description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:image" content="https://moveit.hugorodrigues.ml/"/>
-      <meta property="og:image:type" content="image/svg+xml"/>
-      <meta property="og:image:width" content="1200"/>
-      <meta property="og:image:height" content="630"/>
-
-      <meta name="twitter:url" content="https://moveit.hugorodrigues.ml/"/>
-      <meta name="twitter:card" content="summary_large_image"/>
-      <meta name="twitter:title" content="Moveit"/>
-      <meta name="twitter:description" content="Inspirado na Técnica Pomodoro, ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-      <meta name="twitter:image" content="https://moveit.hugorodrigues.ml/"/>  
-    </Helmet>
-    </>
+    </div>
 
   )
 }
