860bef9822435142ffb9d76808c8e05593c0a57a
Hugo <mailvitorhugosr@gmail.com>
2021-04-12 16:22:48 +0000

    Fix 'absolute url'

--- moveit-whats/src/pages/_document.tsx
+++ moveit-whats/src/pages/_document.tsx
@@ -19,14 +19,14 @@ export default class MyDocument extends Document {
 
           <meta itemProp="name" content="Move.it"/>
           <meta itemProp="description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-          <meta itemProp="image" content="/banner.png"/>
+          <meta itemProp="image" content="https://moveit.hugorodrigues.ml/banner.png"/>
 
           <meta property="og:url" content="https://moveit.hugorodrigues.ml/"/>
           <meta property="og:site_name" content="Moveit"/>
           <meta property="og:title" content="Moveit"/>
           <meta property="og:description" content="Ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
           <meta property="og:type" content="website"/>
-          <meta property="og:image" content="/banner.png"/>
+          <meta property="og:image" content="https://moveit.hugorodrigues.ml/banner.png"/>
           <meta property="og:image:type" content="image/png"/>
           <meta property="og:image:width" content="1200"/>
           <meta property="og:image:height" content="630"/>
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
           <meta name="twitter:card" content="summary_large_image"/>
           <meta name="twitter:title" content="Moveit"/>
           <meta name="twitter:description" content="Inspirado na Técnica Pomodoro, ajuda você a se concentrar nas tarefas e gerenciar seu tempo."/>
-          <meta name="twitter:image" content="/banner.png"/>  
+          <meta name="twitter:image" content="https://moveit.hugorodrigues.ml/banner.png"/>  
           
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
